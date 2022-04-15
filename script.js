db2ba68a81af6b41c26d0c50210f4784a3647111
nakibnahed <nahednakibyos@gmail.com>
2022-04-15 17:34:47 +0300

    "last style"

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function myfunction() {
         return a = 1;
     }
 }
+
+var b;
+
+function showmenu() {
+    if (b == 1) {
+        document.getElementById("navbar").style.display = "none";
+        return b = 0;
+    } else {
+        document.getElementById("navbar").style.display = "inline";
+        return b = 0;
+    }
+};
