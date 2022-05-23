9a8e3ff693d2e8bbbf6ff7d2e6fb0f8128a707a8
nakibnahed <nahednakibyos@gmail.com>
2022-05-23 20:40:58 +0300

    some style changed

--- script.js
+++ script.js
@@ -14,15 +14,3 @@ function myfunction() {
 function myFunction() {
     alert("I am an alert box!");
 }
-
-/* var b;
-
-function showmenu() {
-    if (b == 1) {
-        document.getElementById("navbar").style.display = "none";
-        return b = 0;
-    } else {
-        document.getElementById("navbar").style.display = "inline";
-        return b = 0;
-    }
-}; */
