6cca9b53ab915fe5a5c672772290153bd465d45a
nakibnahed <99716659+nakibnahed@users.noreply.github.com>
2023-01-28 19:12:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let workTitle = "Web Developer";
+// let workTitle = "Web Developer";
 
 let index = 1;
 
@@ -15,3 +15,4 @@ function writeText() {
 setInterval(() => {
     writeText()
 }, 120, 5);
+  
