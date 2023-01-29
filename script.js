bc93002789f10e4b17c4b90c0884a771a8409d51
nakibnahed <nahednakibyos@gmail.com>
2023-01-29 09:46:56 +0300

    my work add

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
-let workTitle = "Web Developer";
+// let workTitle = "Web Developer";
 
-let index = 1;
+// let index = 1;
 
-function writeText() {
-    document.getElementById("work-title").textContent = workTitle.slice(0, index);
+// function writeText() {
+//     document.getElementById("work-title").textContent = workTitle.slice(0, index);
 
-    index++
+//     index++
 
-    if (index > workTitle.length) {
-        index = 1;
-    }
-}
+//     if (index > workTitle.length) {
+//         index = 1;
+//     }
+// }
 
-setInterval(() => {
-    writeText()
-}, 170, 5);
+// setInterval(() => {
+//     writeText()
+// }, 170, 5);
 
-document.getElementById("myname").style.color = "blue";
+// document.getElementById("myname").style.color = "blue";
+
+
+
+// slider functions 
+
+// var slides = document.querySelectorAll(".card");
+// var dots = document.querySelectorAll(".dot");
+// var index = 0;
+
+
+// function prevSlide(n) {
+//     index += n;
+//     console.log("prevSlide is called");
+//     changeSlide();
+// }
+
+// function nextSlide(n) {
+//     index += n;
+//     changeSlide();
+// }
+
+// changeSlide();
+
+// function changeSlide() {
+
+//     if (index > slides.length - 1)
+//         index = 0;
+
+//     if (index < 0)
+//         index = slides.length - 1;
+
+
+
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+
+//         dots[i].classList.remove("active");
+
+
+//     }
+
+//     slides[index].style.display = "block";
+//     dots[index].classList.add("active");
+
+
+
+// }
