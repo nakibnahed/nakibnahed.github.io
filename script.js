4f048d2f87166204b86ae243522f8c00d4fd60cc
nakibnahed <nahednakibyos@gmail.com>
2023-02-12 15:36:33 +0300

    new section add

---
+++ script.js
@@ -0,0 +1,26 @@
+let slider = tns({
+    container: ".my-slider",
+    "slideBy": 1,
+    "speed": 400,
+    "nav": false,
+    controlsContainer: "#controls",
+    prevButton: ".previous",
+    nextButton: ".next",
+    responsive: {
+        1600: {
+            items: 3,
+            gutter: 30
+        },
+        1024: {
+            items: 3,
+            gutter: 20
+        },
+        768: {
+            items: 2,
+            gutter: 20
+        },
+        480: {
+            items: 1
+        }
+    }
+})
