af9fbe6356f296aaa254b6804c54f7723744f9c9
nakibnahed <nahednakibyos@gmail.com>
2023-02-18 13:54:35 +0300

    style change

--- script.js
+++ script.js
@@ -28,3 +28,45 @@ let slider = tns({
         }
     }
 })
+
+let slider_2 = tns({
+    container: ".my-slider-2",
+    "slideBy": 1,
+    "speed": 400,
+    "nav": false,
+    "dots": true,
+    "mouseDrag": true,
+    "touchDrag": true,
+    "loop": true,
+    controlsContainer: ".controls-2",
+    prevButton: ".previous-2",
+    nextButton: ".next-2",
+    responsive: {
+        1600: {
+            items: 3,
+            gutter: 30
+        },
+        1024: {
+            items: 3,
+            gutter: 20
+        },
+        768: {
+            items: 2,
+            gutter: 20
+        },
+        480: {
+            items: 1
+        }
+    }
+})
+
+
+const navEl = document.querySelector('.new-navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 56) {
+        navEl.classList.add('navbar-scrolled');
+    } else if (window.scrollY < 56) {
+        navEl.classList.remove('navbar-scrolled');
+    }
+})
