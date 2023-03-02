6dfec969a1e4292929db06fd79a50abaa8dfd6d1
nakibnahed <nahednakibyos@gmail.com>
2023-03-02 15:54:41 +0300

    style change

--- script.js
+++ script.js
@@ -62,12 +62,12 @@ let slider_2 = tns({
 })
 
 
-const navEl = document.querySelector('.new-navbar');
+// const navEl = document.querySelector('.new-navbar');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 56) {
-        navEl.classList.add('navbar-scrolled');
-    } else if (window.scrollY < 56) {
-        navEl.classList.remove('navbar-scrolled');
-    }
-})
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY >= 56) {
+//         navEl.classList.add('navbar-scrolled');
+//     } else if (window.scrollY < 56) {
+//         navEl.classList.remove('navbar-scrolled');
+//     }
+// })
