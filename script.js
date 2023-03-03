19c5dc1f6f756b498de7ddda2050d1a76d76d7de
nakibnahed <nahednakibyos@gmail.com>
2023-03-03 13:59:40 +0300

    style

--- script.js
+++ script.js
@@ -1,66 +1,64 @@
 let slider = tns({
-    container: ".my-slider",
-    "slideBy": 1,
-    "speed": 400,
-    "nav": false,
-    "dots": true,
-    "mouseDrag": true,
-    "touchDrag": true,
-    "loop": true,
-    controlsContainer: ".controls",
-    prevButton: ".previous",
-    nextButton: ".next",
-    responsive: {
-        1600: {
-            items: 3,
-            gutter: 30
-        },
-        1024: {
-            items: 3,
-            gutter: 20
-        },
-        768: {
-            items: 2,
-            gutter: 20
-        },
-        480: {
-            items: 1
-        }
-    }
-})
+  container: ".my-slider",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  dots: true,
+  mouseDrag: true,
+  touchDrag: true,
+  loop: true,
+  controlsContainer: ".controls",
+  prevButton: ".previous",
+  nextButton: ".next",
+  responsive: {
+    1600: {
+      items: 3,
+      gutter: 30,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    768: {
+      items: 2,
+      gutter: 20,
+    },
+    480: {
+      items: 1,
+    },
+  },
+});
 
 let slider_2 = tns({
-    container: ".my-slider-2",
-    "slideBy": 1,
-    "speed": 400,
-    "nav": false,
-    "dots": true,
-    "mouseDrag": true,
-    "touchDrag": true,
-    "loop": true,
-    controlsContainer: ".controls-2",
-    prevButton: ".previous-2",
-    nextButton: ".next-2",
-    responsive: {
-        1600: {
-            items: 3,
-            gutter: 30
-        },
-        1024: {
-            items: 3,
-            gutter: 20
-        },
-        768: {
-            items: 2,
-            gutter: 20
-        },
-        480: {
-            items: 1
-
-        }
-    }
-})
-
+  container: ".my-slider-2",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  dots: true,
+  mouseDrag: true,
+  touchDrag: true,
+  loop: true,
+  controlsContainer: ".controls-2",
+  prevButton: ".previous-2",
+  nextButton: ".next-2",
+  responsive: {
+    1600: {
+      items: 3,
+      gutter: 30,
+    },
+    1024: {
+      items: 3,
+      gutter: 20,
+    },
+    768: {
+      items: 2,
+      gutter: 20,
+    },
+    480: {
+      items: 1,
+    },
+  },
+});
 
 // const navEl = document.querySelector('.new-navbar');
 
