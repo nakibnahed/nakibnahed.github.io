429e7496970716389b661febc7364ebf31217aa0
nakibnahed <nahednakibyos@gmail.com>
2023-04-10 21:26:25 +0300

    kj

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ let slider_2 = tns({
   dots: true,
   mouseDrag: true,
   touchDrag: true,
-  loop: true,
+  loop: false,
   controlsContainer: ".controls-2",
   prevButton: ".previous-2",
   nextButton: ".next-2",
@@ -60,6 +60,46 @@ let slider_2 = tns({
   },
 });
 
+const jsonInfo = (data) => {
+  // services json info
+  document.querySelector("#WebsiteTitle").innerText =
+    data.services.website.website_title;
+  document.querySelector("#WebsiteDescription").innerText =
+    data.services.website.website_description;
+
+  document.querySelector("#WordpressTitle").innerText =
+    data.services.wordpress.wordpress_title;
+  document.querySelector("#WordpressDescription").innerText =
+    data.services.wordpress.wordpress_description;
+
+  document.querySelector("#SEOTitle").innerText = data.services.seo.seo_title;
+  document.querySelector("#SEODescription").innerText =
+    data.services.seo.seo_description;
+
+  // modal services json info
+  document.querySelector("#WebsiteTitleModal").innerText =
+    data.modal_services.website_modal.website_title_modal;
+  document.querySelector("#WebsiteDescriptionModal").innerText =
+    data.modal_services.website_modal.website_description_modal;
+
+  document.querySelector("#WordpressTitleModal").innerText =
+    data.modal_services.wordpress_modal.wordpress_title_modal;
+  document.querySelector("#WordpressDescriptionModal").innerText =
+    data.modal_services.wordpress_modal.wordpress_description_modal;
+
+  document.querySelector("#SEOTitleModal").innerText =
+    data.modal_services.seo_modal.seo_title_modal;
+  document.querySelector("#SEODescriptionModal").innerText =
+    data.modal_services.seo_modal.seo_description_modal;
+
+  // about me json info
+  document.querySelector("#aboutMe").innerText = data.about.about_me;
+};
+
+fetch("info.json")
+  .then((response) => response.json())
+  .then(jsonInfo);
+
 // const navEl = document.querySelector('.new-navbar');
 
 // window.addEventListener('scroll', () => {
@@ -69,9 +109,3 @@ let slider_2 = tns({
 //         navEl.classList.remove('navbar-scrolled');
 //     }
 // })
-
-fetch("info.json")
-  .then((response) => response.json())
-  .then((data) => {
-    document.querySelector("#aboutMe").innerText = data.about;
-  });
